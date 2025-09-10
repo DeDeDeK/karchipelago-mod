05c41df86c8573774c972e43316cc5937757724f
DeDeDK <216604342+DeDeDeK@users.noreply.github.com>
2025-09-10 14:29:38 -0600

    Initial commit

---
+++ examples/scene/src/main.c
@@ -0,0 +1,10 @@
+#include "hoshi/settings.h"
+
+char ModName[] = "Scene Example"; // Name of the mod.
+char ModAuthor[] = "Your Name";   // Creator of the mod.
+char ModVersion[] = "v1.0";       // Version of the mod.
+
+// To-do
+void OnBoot()
+{
+}
